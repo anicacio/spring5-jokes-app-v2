3993d8b556b59caf6248e4c328b5609797325f5c
Anderson <anderson@nicacio.com.br>
2021-04-27 01:13:09 -0300

    Add Template index. Close #4.

--- src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -17,6 +17,6 @@ public class JokeController {
     @RequestMapping({"/",""})
     public  String showJoke(Model model) {
         model.addAttribute("joke", jokeService.getJoke());
-        return "index"
+        return "index";
     }
 }
